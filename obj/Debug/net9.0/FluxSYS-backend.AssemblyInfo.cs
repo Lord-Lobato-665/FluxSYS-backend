168d2f3b2882eae1ea2848ababb4854020b32b99
Gael Lobato <gaelsanchez509@gmail.com>
2025-03-18 18:56:57 -0500

    lauchSettings configured

--- obj/Debug/net9.0/FluxSYS-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/FluxSYS-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40773986244c27065599ee7065aa374cc8c6f19c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8adb830c08b878fa271e932fdc4ee7332a9a9ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
