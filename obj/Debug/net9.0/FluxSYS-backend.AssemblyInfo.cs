f8c1678d4d2392e091fb9751339605fa94b6424d
Gael Lobato <gaelsanchez509@gmail.com>
2025-03-19 12:24:38 -0500

    movements types by company

--- obj/Debug/net9.0/FluxSYS-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/FluxSYS-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c88010683df5f49b9d439f99325c19cc0e4ac16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2bd30cd08df6491bf3256329d4d1908fbb49ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FluxSYS-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
