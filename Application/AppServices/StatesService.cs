c5230cf1275774e08c96699fbbb8d3002edd340d
Gael Lobato <gaelsanchez509@gmail.com>
2025-03-21 13:28:14 -0500

    states with transactions

--- Application/AppServices/StatesService.cs
+++ Application/AppServices/StatesService.cs
@@ -1,15 +1,26 @@
 ﻿using FluxSYS_backend.Application.DTOs.States;
 using FluxSYS_backend.Domain.IServices;
+using FluxSYS_backend.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FluxSYS_backend.Application.Services
 {
     public class StatesService : IStates
     {
         private readonly IStates _repository;
+        private readonly ApplicationDbContext _context;
+        private readonly ErrorLogService _errorLogService;
 
-        public StatesService(IStates repository)
+        public StatesService(
+            IStates repository,
+            ApplicationDbContext context,
+            ErrorLogService errorLogService)
         {
             _repository = repository;
+            _context = context;
+            _errorLogService = errorLogService;
         }
 
         public async Task<IEnumerable<StateReadDTO>> GetAllAsyncStates()
@@ -24,22 +35,74 @@ namespace FluxSYS_backend.Application.Services
 
         public async Task AddAsyncState(StateCreateDTO dto)
         {
-            await _repository.AddAsyncState(dto);
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _repository.AddAsyncState(dto);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    await _errorLogService.SaveErrorAsync(ex.Message, ex.StackTrace, "AddAsyncState");
+                    throw;
+                }
+            }
         }
 
         public async Task UpdateAsyncState(int id, StateUpdateDTO dto)
         {
-            await _repository.UpdateAsyncState(id, dto);
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _repository.UpdateAsyncState(id, dto);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    await _errorLogService.SaveErrorAsync(ex.Message, ex.StackTrace, "UpdateAsyncState");
+                    throw;
+                }
+            }
         }
 
         public async Task SoftDeleteAsyncState(int id)
         {
-            await _repository.SoftDeleteAsyncState(id);
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _repository.SoftDeleteAsyncState(id);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    await _errorLogService.SaveErrorAsync(ex.Message, ex.StackTrace, "SoftDeleteAsyncState");
+                    throw;
+                }
+            }
         }
 
         public async Task RestoreAsyncState(int id)
         {
-            await _repository.RestoreAsyncState(id);
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _repository.RestoreAsyncState(id);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    await _errorLogService.SaveErrorAsync(ex.Message, ex.StackTrace, "RestoreAsyncState");
+                    throw;
+                }
+            }
         }
     }
 }
